c731ad2dff465f3a519546a4360d91bf6753094b
cyberscribbleorg <pragash.jeyaratnam@gmail.com>
2024-06-14 14:28:07 -0400

    Fixed envioronment variables inconsistencies

--- superset/wait_for_it.sh
+++ superset/wait_for_it.sh
@@ -7,10 +7,11 @@ host="$1"
 shift
 cmd="$@"
 
-until PGPASSWORD=$DB_PASS psql -h "$host" -U "$DB_USER" -d "$DB_NAME" -c '\q'; do
+until PGPASSWORD=$DB_PASS psql -h "$host" -U "$DB_USER" -d "$DB_NAME" -p "$DB_PORT" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
+
 
 >&2 echo "Postgres is up - executing command"
 exec $cmd
